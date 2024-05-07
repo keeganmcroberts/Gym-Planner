4b7dd2d030caca9a21258299f9004e31f4021ce4
Keegan McRoberts <keegan.mcroberts@gmail.com>
2024-05-07 10:42:53 -0600

    adding form to submit daily workout

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home from './components /home';
+import Calendar from './components /calendar';
 
 function App() {
   return (
--- client/src/components /home.js
+++ client/src/components /home.js
@@ -1,7 +1,39 @@
-function Home(){
-    return(
-        <h1>Home</h1>
-    )
+import {useState} from 'react'
+import Calendar from './calendar';
+
+
+function Home() {
+
+    const [workoutlist, setWorkoutList] = useState([{workout: ''}, {workout: ''}])
+
+    function addWorkout(){
+
+    }
+
+  return (
+    <div className="card">
+        <form className="workout-form">
+            <h2>Date:</h2>
+                <input></input>
+            <h2>Day:</h2>
+                <input></input>
+            <h2>Muscle Group:</h2>
+                <input></input>
+            <h2>Workouts:</h2>
+            {workoutlist.map((singleWorkout, index) => (
+            <div key="index" className="workouts">
+                <input></input>
+                {workoutlist.length - 1 === index && 
+                (
+                <button>Add</button>
+                )}
+            </div>
+            ))}
+            <h2>Notes:</h2>
+                <input></input>
+        </form>
+    </div>
+  );
 }
 
 export default Home;
