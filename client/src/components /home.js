f801560bb2d740bc79c43b619a071fa75c867df3
Keegan McRoberts <keegan.mcroberts@gmail.com>
2024-05-07 11:31:28 -0600

    handleChange function

--- client/src/components /home.js
+++ client/src/components /home.js
@@ -4,15 +4,24 @@ import Calendar from './calendar';
 
 function Home() {
 
+    
     const [workoutlist, setWorkoutList] = useState([{workout: ''}])
+    console.log("workout list", workoutlist)
 
     const addWorkout = () => {
-        setWorkoutList([...workoutlist, {service: ""}])
+        setWorkoutList([...workoutlist, {workout: ""}])
     }
 
     const removeWorkout = (index) =>{
         const list = [...workoutlist]
         list.splice(index, 1)
+        setWorkoutList(list)
+    }
+
+    const handleInputChange = (e, index) => {
+        const {name, value} = e.target 
+        const list = [...workoutlist]
+        list[index][name] = value;
         setWorkoutList(list)
     }
 
@@ -27,14 +36,24 @@ function Home() {
                 <input></input>
             <h2>Workouts:</h2>
             {workoutlist.map((singleWorkout, index) => (
-            <div key="index" className="workouts">
-                <input></input>
-                <button onClick={()=>removeWorkout(index)}>Remove</button>
-                <br></br>
-                {workoutlist.length - 1 === index && 
+            <div key={index} className="workouts">
+                <input
+                id='workout'
+                name='workout'
+                type='text'
+                value={singleWorkout.workout}
+                onChange={(e) => handleInputChange(e, index)}
+                ></input>
+                {workoutlist.length - 1 === index && workoutlist.length < 5 &&
                 (
                 <button onClick={addWorkout}>Add</button>
                 )}
+                {workoutlist.length > 1 && ( 
+                <button onClick={()=>removeWorkout(index)}>Remove</button>
+                )}
+
+                <br></br>
+
             </div>
             ))}
             <h2>Notes:</h2>
